03de843bee977e7057a2ca3c3e7d0d299cdcee85
deadbacsi <deadcsavo@gmail.com>
2018-10-04 12:47:41 +0200

    major reworks on enemy

--- globalLogic.py
+++ globalLogic.py
@@ -1,8 +1,10 @@
 import level, os, common, player, time, sys, keyboard, echo, copy, color, enemy
 
 def main(dev=0):
+    dev = 0
     level.load_level(sys.path[0]+'/maps/devMap')
     level.paint_vision()
+    enemy.spawn(3,3,0,5)
     while(1):
         key = keyboard.getch()
         os.system('clear')
@@ -16,22 +18,22 @@ def main(dev=0):
             print('Debug: D')
             player.turn("right")
         if key == "f":
+            print('Debug: F')
             player.attack()
+        if key == "r":
+            enemy.enemyAction()
         if key == '0':
             break
         if dev == 0:    # def mode 0
-            level.paint_vision()
+            level.paint_level()
+            print(f'Facing: {player.facing}')
+            print(f'Player Health: {player.health}')
         else:           # dev mode 1
-            if player.x == enemy.m.x & player.y == enemy.m.y:
-                print('KABUUUMM')
-                enemy.m = ''
             devMap = copy.deepcopy(level.world)
             devMap[player.y][player.x] = (color.green+'T'+color.white)
-            devMap[enemy.m.y][enemy.m.x] = (color.red+'E'+color.white)
+            for monster in enemy.enemies:
+                devMap[monster.y][monster.x] = (color.red+'E'+color.white)
             for i in devMap:
                 print(''.join(i))
-            print(f'Player Health: {player.health}')
-            print(f'Enemy Health: {enemy.enemies[0].health}')
-            print(f'Facing: {player.facing}')
     print(str(player.x) + '-' + str(player.y))
     return 1
--- player.py
+++ player.py
@@ -25,19 +25,48 @@ def turn(direction):
         if direction in ['left', 'bal', 'l']:
             if facing == 0:
                 facing = 3
+                enemy.enemyAction()
             else:
                 facing -= 1
+                enemy.enemyAction()
         if direction in ['right', 'jobb', 'r']:
             if facing == 3:
                 facing = 0
+                enemy.enemyAction()
             else:
                 facing += 1
+                enemy.enemyAction()
     else:
         facing = direction
     return
 
 def attack():
-    pass
+    if facing == 0:
+        if isMonster(x, y+1):
+            print('Debug: it hit!')
+            damageMonster(x,y+1)
+    elif facing == 1:
+        if isMonster(x+1, y):
+            print('Debug: it hit!')
+            damageMonster(x+1,y)
+    elif facing == 2:
+        if isMonster(x, y-1):
+            print('Debug: it hit!')
+            damageMonster(x,y-1)
+    elif facing == 3:
+        if isMonster(x-1, y):
+            print('Debug: it hit!')
+            damageMonster(x-1,y)
+
+def damageMonster(newX, newY):
+    enemy.enemyAction()
+    for monster in enemy.enemies:
+        if newX == monster.x and newY == monster.y:
+            monster.health -= 1
+            if monster.health == 0:
+                enemy.enemies.remove(monster)
+            break
+    return
 
 def move(direction):
     global x,y
@@ -46,18 +75,22 @@ def move(direction):
             if not isMonster(x, y-1):
                 if isPassable(x, y-1):
                     y-=1
+                    enemy.enemyAction()
         elif facing == 1:
             if not isMonster(x+1, y):
                 if isPassable(x+1, y):
                     x+=1
+                    enemy.enemyAction()
         elif facing == 2:
             if not isMonster(x, y+1):
                 if isPassable(x, y+1):
                     y+=1
+                    enemy.enemyAction()
         elif facing == 3:
             if not isMonster(x-1, y):
                 if isPassable(x-1, y):
                     x-=1
+                    enemy.enemyAction()
         else:
             common.printErr('Wrong facing')
     return
